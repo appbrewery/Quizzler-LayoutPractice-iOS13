ca6b9f656e6dd504424d75860c4a0744633e9d5e
Nico Cobelo <ingcobelo@gmail.com>
2020-10-23 17:15:11 -0400

    Created a 2d array

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -16,9 +16,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let quiz = [
-        "Four + Two is equal to Six",
-        "Five - Three is greater than One",
-        "Three + Eight is less than Ten"
+        ["Four + Two is equal to Six","True"],
+        ["Five - Three is greater than One","True"],
+        ["Three + Eight is less than Ten", "False"],
+        
     ]
     
     var questionNumber = 0
@@ -31,12 +32,27 @@ class ViewController: UIViewController {
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        questionNumber += 1
-        updateUI()
+        
+        let userAnswer = sender.currentTitle  //True, False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+            updateUI()
     }
  
     func updateUI() {
-        questionLabel.text = quiz[questionNumber]
+        questionLabel.text = quiz[questionNumber][0]
     }
 }
 
