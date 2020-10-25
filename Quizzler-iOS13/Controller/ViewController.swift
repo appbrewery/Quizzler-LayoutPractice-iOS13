2ee9df7c6dab21c3cf5b8d1aeece1c970bfef07c
Nico Cobelo <ingcobelo@gmail.com>
2020-10-25 10:47:10 -0400

    Added score label

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
@@ -45,7 +46,7 @@ class ViewController: UIViewController {
     @objc func updateUI() {
         questionLabel.text = quizBrain.getQuestionText()
         progressBar.progress = quizBrain.getProgress()
-        
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
     }
--- Quizzler-iOS13/Model/QuizBrain.swift
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -25,13 +25,19 @@ struct QuizBrain {
     ]
     
     var questionNumber = 0
+    var score = 0
     
-    func checkAnswer(_ userAnswer: String) -> Bool{
+    mutating func checkAnswer(_ userAnswer: String) -> Bool{
         if userAnswer == quiz[questionNumber].answer {
+            score += 1
             return true
         } else {
             return false
         }
+    }
+    
+    func getScore() -> Int {
+        return score
     }
     
     func getQuestionText() -> String {
@@ -43,11 +49,12 @@ struct QuizBrain {
         return progress
     }
     
-    func nextQuestion() {
+    mutating func nextQuestion() {
         if questionNumber + 1 < quiz.count {
             questionNumber += 1
         } else {
             questionNumber = 0
+            score = 0
         }
     }
 }
